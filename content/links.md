bcadf7e00a2ad4ef00dd0d7c8c4f9030a44af068
mdb1710 <mdb1710@gmail.com>
2020-05-01 11:09:11 -0400

    created links page

---
+++ content/links.md
@@ -0,0 +1,13 @@
+---
+title: Links
+date: 2020-05-01T19:31:20.591Z
+---
+
+## Find Us On the Internets...
+
+- Twitter
+- Instagram 
+- Facebook
+- Youtube (Coming Soon)
+- Snapchat (Coming Soon)
+- TikTok (Coming Soon)
---
+++ pages/links.js
@@ -0,0 +1,18 @@
+import Layout from '../components/layout';
+import { attributes, html } from '..content/links.md';
+
+
+const Links = () => {
+    <Layout>
+      <h1>{attributes.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
+      <style jsx>{`
+       h1,
+      div {
+        text-align: center;
+      }
+      `}</style>
+    </Layout>;
+}
+
+export default Links
